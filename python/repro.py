22b7cb41b015675cf3c05e46bab6bffd180dce2f
Victor van Kempen <victor.van-kempen@insa-rennes.fr>
2024-10-21 08:50:02 +0200

    Changed number of repetitions from 1M to 100 to analyse the impacts

--- python/repro.py
+++ python/repro.py
@@ -1,26 +1,36 @@
 from random import *
 
-def testTrans(x,y,z):
-    a = x+y
-    b = y+z
-    return a+z==x+b
-    
+
+def testTrans(x, y, z):
+    a = x + y
+    b = y + z
+    return a + z == x + b
+
+
 def runAlea():
-    x = random() * randint(0,10000000000)
-    y = random() * randint(0,10000000000)
-    z = random() * randint(0,10000000000)
-    return testTrans(x,y,z)
-    
+    x = random() * randint(0, 10000000000)
+    y = random() * randint(0, 10000000000)
+    z = random() * randint(0, 10000000000)
+    return testTrans(x, y, z)
+
+
 def test(n):
     j = 0
     k = 0
     for i in range(n):
         if not runAlea():
-            j = j+1
-        #if i%100000 == 0 and i>0:
-            #print("\nresultats partiels :\ntours :{}\nerreur : {}\ntaux : {}".format(i,j,j/i))
-    return j/n
-    
-if __name__ == "__main__" : 
-    res = test(1000000)
-    print("x+(y+z) == (x+y)+z {}% of cases".format((1-res)*100))
+            j = j + 1
+        if i % 100000 == 0 and i > 0:
+            print("\nresultats partiels :\ntours :{}\nerreur : {}\ntaux : {}".format(i, j, j / i))
+    return j / n
+
+
+if __name__ == "__main__":
+    taux = []
+    for i in range(100):
+        res = test(100)
+        taux.append(res)
+    print("x+(y+z) == (x+y)+z approximately {}% of cases".format((1 - res) * 100))
+    print("Taux d'erreur :", taux)
+    print("Taux d'erreur max :", max(taux))
+    print("Taux d'erreur min :", min(taux))
